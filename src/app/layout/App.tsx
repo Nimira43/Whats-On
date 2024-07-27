7cb0316c60e1cb0614c3883cfe0bfdbf8f9b6144
Nick Rabone <n.rabone72@aol.com>
2024-07-27 13:40:50 +0100

    Working on Navbar.jsx

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,16 @@
+import { Container } from "semantic-ui-react"
 import EventDashboard from "../../features/events/dashboard/EventDashboard"
+import NavBar from "./nav/NavBar"
 
 function App() {
   
   return (
-    <>
-      <h1>What's On</h1>
-      <EventDashboard />
-    </>
+    <div>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>  
+    </div>
   )
 }
 
