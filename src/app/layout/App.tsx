3b1428566adf8b2a470f389814c378e9a4d54dfd
Nick Rabone <n.rabone72@aol.com>
2024-07-27 12:58:17 +0100

    Created EventDashboard.jsx file

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,8 +1,11 @@
+import EventDashboard from "../../features/events/dashboard/EventDashboard"
+
 function App() {
   
   return (
     <>
       <h1>What's On</h1>
+      <EventDashboard />
     </>
   )
 }
