7f61c41988e8581249bc592da6903d545ea71afd
Nick Rabone <n.rabone72@aol.com>
2024-07-27 16:43:02 +0100

    Made sample data

---
+++ sampleData.ts
@@ -0,0 +1,46 @@
+export const sampleData=[
+  {
+    id: '1',
+    title: 'Visit Stonehenge',
+    date: '2024-08-25',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, debitis id tenetur dolore deserunt culpa, atque quos dolorum et ut totam quisquam iure sit eligendi.',
+    city: 'Near Amesbury, UK',
+    venue: 'Stonehenge, Wiltshire, SP4 7DE, UK',
+    hostedBy: 'Steve',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/63.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Helen',
+        photoURL: 'https://randomuser.me/api/portraits/women/32.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Thomas',
+        photoURL: 'https://randomuser.me/api/portraits/men/37.jpg',
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: 'Explore Edinburgh Castle',
+    date: '2024-09-02',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, debitis id tenetur dolore deserunt culpa, atque quos dolorum et ut totam quisquam iure sit eligendi.',
+    city: 'Edinburgh, UK',
+    venue: 'Castlehill, Edinburgh, Scotland, UK, EH1 2NG, UK',
+    hostedBy: 'Rachel',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/women/43.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Paul',
+        photoURL: 'https://randomuser.me/api/portraits/men/29.jpg',
+      },
+      {
+        id: 'b',
+        name: 'Louisa',
+        photoURL: 'https://randomuser.me/api/portraits/men/8.jpg',
+      }
+    ]
+  },
+]
